dfb07abf029293bfbf05e17e1400f597a84c8097
Pedro Azevedo <pedrolsazevedo@gmail.com>
2023-04-23 00:57:33 -0300

    feat(install.sh) Added Gum and Fixed ansible folder and host

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define Gum style colors
+GUM_YELLOW='\033[1;33m'
+GUM_GREEN='\033[1;32m'
+GUM_BLUE='\033[1;34m'
+GUM_RESET='\033[0m'
+
 declare -a packages=("python3" "ansible" "python3-pip" "ansible-lint")
 declare -a install_packages=()
 
@@ -10,49 +16,51 @@ for package in "${packages[@]}"; do
 done
 
 if [ ${#install_packages[@]} -eq 0 ]; then
-    echo "All required packages are already installed."
+    echo -e "${GUM_GREEN}All required packages are already installed.${GUM_RESET}"
 else
-    echo "Installing the following packages: ${install_packages[@]}"
+    echo -e "${GUM_BLUE}Installing the following packages:${GUM_RESET} ${install_packages[@]}"
     sudo apt-get update
     for package in "${install_packages[@]}"; do
       if [ "$package" != "python3-pip" ] || [ ! -x "$(command -v pip)" ]
       then
           sudo apt-get install -y "$package"
       else
-          echo "Skipping installation of python3-pip since pip is already installed."
+          echo -e "${GUM_YELLOW}Skipping installation of python3-pip since pip is already installed.${GUM_RESET}"
       fi
     done
 fi
 
 # Check and Create Ansible inventory and hosts file
 if [ ! -d "/etc/ansible" ]; then
-    mkdir -p /etc/ansible
+    echo -e "${GUM_BLUE}Ansible directory doesn't exists.${GUM_RESET}"
+    sudo mkdir -p /etc/ansible
 fi
 
 if [ ! -f "/etc/ansible/hosts" ]; then
-    echo "[local]" > /etc/ansible/hosts
-    echo "localhost ansible_connection=local" >> /etc/ansible/hosts
+    echo -e "${GUM_BLUE}Ansible hosts doesn't exists.${GUM_RESET}"
+    echo "[local]" | sudo tee -a /etc/ansible/hosts
+    echo "localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts
 else
     if ! grep -q "\[local\]" /etc/ansible/hosts || ! grep -q "localhost ansible_connection=local" /etc/ansible/hosts; then
-        echo -e "\n[local]" >> /etc/ansible/hosts
-        echo "localhost ansible_connection=local" >> /etc/ansible/hosts
+        echo -e "${GUM_YELLOW}Updating Ansible hosts file.${GUM_RESET}"
+        echo -e "\n[local]" | sudo tee -a /etc/ansible/hosts
+        echo "localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts
     fi
 fi
 
 # Start basic configuration
-echo "Starting Ansible configuration"
-read -p "Install basic packages (vim, wget, Docker, Visual Studio Code, Google Chrome, Homebrew)? (y/n): " basic_config
+echo -e "${GUM_BLUE}Starting Ansible configuration.${GUM_RESET}"
+read -p "Install basic packages (vim, wget, Docker, Visual Studio Code, Google Chrome, Homebrew, TFENV (brew), Terraform and cz cli)? (y/n): " basic_config
 if [[ $basic_config =~ ^[Yy]$ ]]; then
     ansible-playbook playbook.yml -K --extra-vars "current_user=$(logname)"
 fi
 
 
-# Validade if the user want to install container extra (az cli, az copy and azure storage explorer)
-read -p "Install Azure tools (az cli, az copy and azure storage explorer)? (y/n): " azure_tools
+# Validate if the user wants to install Azure tools (az cli, az copy, and azure storage explorer)
+read -p "${GUM_BLUE}Install Azure tools (az cli, az copy and azure storage explorer)?${GUM_RESET} (y/n): " azure_tools
 if [[ $azure_tools =~ ^[Yy]$ ]]; then
     ansible-playbook -K azure_tools.yml
 fi
-
 
 # Validade if want to install container extra (rancher-desktop and podman-desktop)
 read -p "Install container extra(kubectl, rancher-desktop and podman-desktop)? (y/n): " container_extras
